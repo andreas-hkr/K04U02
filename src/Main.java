d825dbeff4c239c11d9a4322358f9658ed05548d
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 19:53:16 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Beräkna en cirkels omkrets (om inläst radie <= 0 --> felmeddelande)
+        //   Omkrets = 2 * π * radie
+        //   Area = π * radie * radie
+
+
+    }
+}
